66b1b89ad6f3d6babd62c10b7a5c25415fa14d4e
andreaalf97 <andrea.alfieri97@gmail.com>
2020-03-04 09:55:20 +0100

    completed bow extraction with reduced vocabulary of only hedging and negation words

--- feature_extraction/headline_features/hedging_negation_bow.py
+++ feature_extraction/headline_features/hedging_negation_bow.py
@@ -2,12 +2,74 @@ from data_reading.preprocess_data import apply_lower_case, apply_lemmatization
 from data_reading.read_data import read_clean_dataset, PICKLED_FEATURES_PATH
 
 import pandas as pd
-import sklearn
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.corpus import stopwords
+
+# List of words taken from github repo of the paper
+refutingWords = [
+                        'fake',
+                        'fraud',
+                        'hoax',
+                        'false',
+                        'deny', 'denies',
+                        'refute',
+                        'not',
+                        'despite',
+                        'nope',
+                        'doubt', 'doubts',
+                        'bogus',
+                        'debunk',
+                        'pranks',
+                        'retract'
+]
 
 
+hedgingWords = [
+        'alleged', 'allegedly',
+        'apparently',
+        'appear', 'appears',
+        'claim', 'claims',
+        'could',
+        'evidently',
+        'largely',
+        'likely',
+        'mainly',
+        'may', 'maybe', 'might',
+        'mostly',
+        'perhaps',
+        'presumably',
+        'probably',
+        'purported', 'purportedly',
+        'reported', 'reportedly',
+        'rumor', 'rumour', 'rumors', 'rumours', 'rumored', 'rumoured',
+        'says',
+        'seem',
+        'somewhat',
+        'supposedly',
+        'unconfirmed']
 
-def extract_reduced_bow(headlines):
-    return 0, 0
+
+dictionary = refutingWords + hedgingWords
+
+
+#presenceOnly is a trigger for future tests to check if performance improves
+# by analyzing presence of words instead of counts
+def extract_reduced_bow(headlines, useStopwords = False, presenceOnly = False):
+
+    sw = stopwords.words('english') if useStopwords else None
+
+    #CountVectorizer converts a collection of text documents to a matrix of token counts
+    cv = CountVectorizer(
+        input = "content", #The input is expected to be a series of items
+        lowercase = True, #Converts all word to lowercase first --> might not be necessary if we lowercase in advance
+        stop_words = sw,  #the list of stopwords
+        vocabulary = dictionary, #the features
+        binary = presenceOnly, #see above
+    )
+
+    bow = cv.fit_transform(headlines)
+
+    return cv.get_feature_names(), bow
 
 
 
@@ -20,6 +82,7 @@ dataset = apply_lemmatization(dataset)
 
 features, b = extract_reduced_bow(dataset.articleHeadline.values)
 
+
 # create a dataset
 d = pd.DataFrame(b.toarray())
 d.columns = features
@@ -31,4 +94,4 @@ d.columns = features
 # d = create_vectors(dataset, assignments)  # dataframe with all the vectors
 #
 
-d.to_pickle(PICKLED_FEATURES_PATH+"bow.pkl")  # pickle the dataframe to the specified folder
+d.to_pickle(PICKLED_FEATURES_PATH+"ref_hedg_bow.pkl")  # pickle the dataframe to the specified folder
