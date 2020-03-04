dcc4c134996a74b18911993763719bb9d699b8d4
andreaalf97 <andrea.alfieri97@gmail.com>
2020-03-04 11:00:58 +0100

    importing wordlists from root_dist_extractor

--- feature_extraction/headline_features/hedging_negation_bow.py
+++ feature_extraction/headline_features/hedging_negation_bow.py
@@ -5,66 +5,23 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from nltk.corpus import stopwords
 
-# List of words taken from github repo of the paper
-refutingWords = [
-                        'fake',
-                        'fraud',
-                        'hoax',
-                        'false',
-                        'deny', 'denies',
-                        'refute',
-                        'not',
-                        'despite',
-                        'nope',
-                        'doubt', 'doubts',
-                        'bogus',
-                        'debunk',
-                        'pranks',
-                        'retract'
-]
+from feature_extraction.headline_features.root_dist_extractor import _refuting_words, _hedging_words
 
+dictionary = _refuting_words + _hedging_words
 
-hedgingWords = [
-        'alleged', 'allegedly',
-        'apparently',
-        'appear', 'appears',
-        'claim', 'claims',
-        'could',
-        'evidently',
-        'largely',
-        'likely',
-        'mainly',
-        'may', 'maybe', 'might',
-        'mostly',
-        'perhaps',
-        'presumably',
-        'probably',
-        'purported', 'purportedly',
-        'reported', 'reportedly',
-        'rumor', 'rumour', 'rumors', 'rumours', 'rumored', 'rumoured',
-        'says',
-        'seem',
-        'somewhat',
-        'supposedly',
-        'unconfirmed']
-
-
-dictionary = refutingWords + hedgingWords
-
-
-#presenceOnly is a trigger for future tests to check if performance improves
+# presenceOnly is a trigger for future tests to check if performance improves
 # by analyzing presence of words instead of counts
 def extract_reduced_bow(headlines, useStopwords = False, presenceOnly = False):
 
     sw = stopwords.words('english') if useStopwords else None
 
-    #CountVectorizer converts a collection of text documents to a matrix of token counts
+    # CountVectorizer converts a collection of text documents to a matrix of token counts
     cv = CountVectorizer(
-        input = "content", #The input is expected to be a series of items
-        lowercase = True, #Converts all word to lowercase first --> might not be necessary if we lowercase in advance
-        stop_words = sw,  #the list of stopwords
-        vocabulary = dictionary, #the features
-        binary = presenceOnly, #see above
+        input = "content",  # The input is expected to be a series of items
+        lowercase = True,  # Converts all word to lowercase first --> might not be necessary if we lowercase in advance
+        stop_words = sw,  # the list of stopwords
+        vocabulary = dictionary,  # the features
+        binary = presenceOnly,  # see above
     )
 
     bow = cv.fit_transform(headlines)
@@ -72,13 +29,9 @@ def extract_reduced_bow(headlines, useStopwords = False, presenceOnly = False):
     return cv.get_feature_names(), bow
 
 
-
-# Lemmatize the dataset for better representation
 dataset = read_clean_dataset()  # Read the dataset
 dataset = apply_lower_case(dataset)
 dataset = apply_lemmatization(dataset)
-
-
 
 features, b = extract_reduced_bow(dataset.articleHeadline.values)
 
