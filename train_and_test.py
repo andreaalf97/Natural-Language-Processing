c0ae85c9e414145f27982214773fe62d9ab08319
andreaalf97 <andrea.alfieri97@gmail.com>
2020-03-06 14:42:48 +0100

    created and trained Logistic Regression

--- train_and_test.py
+++ train_and_test.py
@@ -1,3 +1,12 @@
 from Model import Model
 
-model = Model("train_and_test", ["bow"], "SVM")
+trainingSettingsLogisticRegression = {
+    "penalty": "l2",  # can be 'l1', 'l2', 'elasticnet', 'none'
+    "max_iter": 100,  # 100 is the default value
+    "n_jobs": -1,  # The number of cores to use, -1 means all
+    "random_state": 0  # The seed for the random number generator used to shuffle the data
+}
+
+model = Model("train_and_test", features=["bow"], classifier="Logistic Regression", trainingSettings=trainingSettingsLogisticRegression)
+
+
