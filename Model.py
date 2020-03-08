404c80da3f435b8b793bb89ef919a6d754eeb5d9
Diego Albo <diego.albo.martinez@gmail.com>
2020-03-08 22:27:03 +0100

    Added best parameters for svm and random forest

--- Model.py
+++ Model.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn import svm
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression, SGDClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import cross_validate, cross_val_predict
 from sklearn.metrics import confusion_matrix
@@ -47,7 +47,6 @@ class Model:
                 df = df['avg_similarity']
 
             finalDF = pd.concat([finalDF, df], axis=1)  # Adds the new columns to the final dataframe
-            print(finalDF.shape)
 
         return finalDF
 
@@ -77,26 +76,28 @@ class Model:
         results = cross_validate(self.model, self.featureMatrix, self.labels,
                                  cv=self.trainingSettings["cross_val_folds"], verbose=1,
                                  scoring=['accuracy', 'f1_weighted', 'precision_weighted', 'recall_weighted',
-                                          'roc_auc_ovr_weighted'])
+                                          'roc_auc_ovr_weighted'], n_jobs=-1)
         results = {k: np.mean(v) for k, v in results.items()}
 
         return results
 
     # Implementation of svm
     def SVM(self):
-        self.model = svm.SVC(gamma=self.trainingSettings["gamma"], kernel=self.trainingSettings["kernel"])
-
+        self.model = svm.SVC(C=self.trainingSettings['C'], gamma=self.trainingSettings["gamma"],
+                             kernel=self.trainingSettings["kernel"], random_state=self.trainingSettings['random_state'],
+                             degree=self.trainingSettings['degree'], probability=True)
         results = cross_validate(self.model, self.featureMatrix, self.labels,
                                  cv=self.trainingSettings["cross_val_folds"], verbose=1,
                                  scoring=['accuracy', 'f1_weighted', 'precision_weighted', 'recall_weighted',
-                                          'roc_auc_ovr_weighted'])
+                                          'roc_auc_ovr_weighted'], n_jobs=-1)
+
         results = {k: np.mean(v) for k, v in results.items()}
 
         return results
 
     # Implementation of logistic regression
     def logisticRegression(self):
-        lrModel = LogisticRegression(
+        self.model = LogisticRegression(
             penalty=self.trainingSettings["penalty"],
             max_iter=self.trainingSettings["max_iter"],
             n_jobs=self.trainingSettings["n_jobs"],
@@ -105,11 +106,14 @@ class Model:
         )
 
         results = \
-            cross_validate(lrModel, self.featureMatrix, self.labels, cv=self.trainingSettings["cross_val_folds"],
+            cross_validate(self.model, self.featureMatrix, self.labels, cv=self.trainingSettings["cross_val_folds"],
                            verbose=1,
                            scoring=['accuracy', 'f1_weighted', 'precision_weighted', 'recall_weighted',
-                                    'roc_auc_ovr_weighted'])
+                                    'roc_auc_ovr_weighted'], n_jobs=-1, return_estimator=True)
 
+        print(results)
+        self.model = results['estimator'][0]
+        del results['estimator']
         results = {k: np.mean(v) for k, v in results.items()}
         return results
 
@@ -117,12 +121,13 @@ class Model:
     def randomForest(self):
         # Initialize the model
         self.model = RandomForestClassifier(
+            n_estimators=self.trainingSettings['n_estimators'],
             max_depth=self.trainingSettings["max_depth"],
             random_state=self.trainingSettings["random_state"]
         )
         results = cross_validate(self.model, self.featureMatrix, self.labels,
                                  cv=self.trainingSettings["cross_val_folds"], verbose=1,
                                  scoring=['accuracy', 'f1_weighted', 'precision_weighted', 'recall_weighted',
-                                          'roc_auc_ovr_weighted'])
+                                          'roc_auc_ovr_weighted'], n_jobs=-1)
         results = {k: np.mean(v) for k, v in results.items()}
         return results
--- train_and_test.py
+++ train_and_test.py
@@ -1,9 +1,10 @@
 from Model import Model
+import numpy as np
 
 trainingSettingsLogisticRegression = {
     "penalty": "l1",  # can be 'l1', 'l2', 'elasticnet', 'none'
     "solver": 'liblinear',
-    "max_iter": 1000,  # 100 is the default value
+    "max_iter": 150,  # 100 is the default value
     "n_jobs": -1,  # The number of cores to use, -1 means all
     "random_state": 0,  # The seed for the random number generator used to shuffle the data
     "cross_val_folds": 10
@@ -15,48 +16,53 @@ trainingSettingsNaiveBayes = {
 }
 
 trainingSettingsSVM = {
+    "C": 2,
+    'random_state': 0,
     "cross_val_folds": 10,
-    "kernel": 'linear',
+    "kernel": 'poly',
+    "degree": 2,
     "gamma": 'scale'
 }
 
 trainingSettingsRandomForest = {
     "cross_val_folds": 10,
-    "max_depth": 6,
+    "max_depth": 5,
+    "n_estimators": 25,
     "random_state": 0
 }
 
 logisticRegressionModel = Model(
     "train_and_test",
-    features=["bow", "q_features", 'length_diff', 'root_dist'],
+    features=['root_dist', 'length_diff', 'q_features'],
     classifier="Logistic Regression",
     settings=trainingSettingsLogisticRegression
 )
 
 naiveBayesModel = Model(
     "train_and_test",
-    features=["bow", "kuhn_munkres", "length_diff", "q_features", "ref_hedg_bow", "root_dist", "SVO_ppdb", "word2vec"],
+    features=['root_dist', 'length_diff', 'q_features'],
     classifier="Naive Bayes",
     settings=trainingSettingsNaiveBayes
 )
 
+print("Accuracy LR: ", logisticRegressionModel.results)
+# print('Coefficients Logistic Regression: ', np.mean(logisticRegressionModel.model.coef_, axis=0))
+
+print("Accuracy NB: ", naiveBayesModel.results)
+
 svmModel = Model(
     "train_and_test",
-    features=["bow", "kuhn_munkres", "length_diff", "q_features", "ref_hedg_bow", "root_dist", "SVO_ppdb", "word2vec"],
+    features=['root_dist', 'length_diff', 'q_features'],
     classifier="SVM",
     settings=trainingSettingsSVM
 )
 
-
 randomForestModel = Model(
     "train_and_test",
-    features=["bow", "kuhn_munkres", "length_diff", "q_features", "ref_hedg_bow", "root_dist", "SVO_ppdb", "word2vec"],
+    features=['root_dist', 'length_diff', 'q_features'],
     classifier="Random Forest",
     settings=trainingSettingsRandomForest
 )
 
-print("Accuracy: ", logisticRegressionModel.results)
-print("Accuracy: ", naiveBayesModel.results)
 print("Accuracy: ", svmModel.results)
 print("Accuracy: ", randomForestModel.results)
-
