c7dc579317bed36d7b1752660cd26f0cc40f9776
Diego Albo <diego.albo.martinez@gmail.com>
2020-03-09 15:08:21 +0100

    Implemented train and test split and return several measure on the test set

--- train_and_test.py
+++ train_and_test.py
@@ -1,4 +1,5 @@
 from Model import Model
+import numpy as np
 
 trainingSettingsLogisticRegression = {
     "penalty": "l1",  # can be 'l1', 'l2', 'elasticnet', 'none'
@@ -36,43 +37,59 @@ nestedRandomForestSettings = {
     "inner_cross_val_folds": 5,
 }
 
+nestedSVMSettings = {
+    "outer_cross_val_folds": 5,
+    "inner_cross_val_folds": 5,
+}
+
 nestedRandomForestGrid = {
     "max_depth": [1, 2, 5, 10, 20, 50],
-    "n_estimators": [10, 15, 25, 50],
+    "n_estimators": [10, 15, 25, 50, 75, 100],
     "random_state": [0],
 }
 
-# logisticRegressionModel = Model(
-#     "train_and_test",
-#     features=["bow", "kuhn_munkres"],
-#     classifier="Logistic Regression",
-#     settings=trainingSettingsLogisticRegression
-# )
+nestedSVMGrid = {
+    "kernel": ['poly'],
+    "C": [0.01, 0.1,0.5, 1, 1.5, 2],
+    'degree': [2, 3],
+    "random_state": [0],
+}
+
+svmModel = Model(
+    "train_and_test",
+    features=["root_dist", "q_features", "length_diff"],
+    classifier="SVM",
+    settings=nestedSVMSettings,
+    hyperparameters_grid=nestedSVMGrid
+
+)
+
+logisticRegressionModel = Model(
+    "train_and_test",
+    features=["root_dist", "q_features", "length_diff"],
+    classifier="Logistic Regression",
+    settings=trainingSettingsLogisticRegression
+)
 #
-# naiveBayesModel = Model(
-#     "train_and_test",
-#     features=["bow", "kuhn_munkres", "length_diff", "q_features", "ref_hedg_bow", "root_dist", "SVO_ppdb", "word2vec"],
-#     classifier="Naive Bayes",
-#     settings=trainingSettingsNaiveBayes
-# )
-#
-# svmModel = Model(
-#     "train_and_test",
-#     features=["bow", "kuhn_munkres", "length_diff", "q_features", "ref_hedg_bow", "root_dist", "SVO_ppdb", "word2vec"],
-#     classifier="SVM",
-#     settings=trainingSettingsSVM
-# )
-#
+naiveBayesModel = Model(
+    "train_and_test",
+    features=["root_dist", "q_features", "length_diff"],
+    classifier="Naive Bayes",
+    settings=trainingSettingsNaiveBayes
+)
+
+
+
 randomForestModel = Model(
     "train_and_test",
-    features=["root_dist"],
+    features=["root_dist", "q_features", "length_diff"],
     classifier="Random Forest",
     settings=nestedRandomForestSettings,
     hyperparameters_grid=nestedRandomForestGrid
 )
 
-# print("Results: ", logisticRegressionModel.results)
-# print("Results: ", naiveBayesModel.results)
-# print("Results: ", svmModel.results)
-print("Results: ", randomForestModel.results)
+print("Results Logistic Regression: ", logisticRegressionModel.results)
+print("Results Naïve Bayes: ", naiveBayesModel.results)
+print("Results SVM: ", svmModel.results)
+print("Results Random Forest: ", randomForestModel.results)
 
